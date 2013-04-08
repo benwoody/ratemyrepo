960d38436d86cf71dcdab0b66c20aec60d307dcb
Ben Woodall <mail@benwoodall.com>
2013-04-08 13:28:41 -0700

    Changed to show repos routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,13 @@ Ratemyrepo::Application.routes.draw do
   match '/auth/:provider/callback', :to => 'sessions#create'
   match '/auth/failure', :to => 'sessions#failure'
 
+  get '/about' => 'pages#about'
+
   get '/settings' => 'settings#index', :as => :settings
   post '/settings' => 'settings#update'
 
-  get '/:username' => 'users#show'
-  get '/:username/:repo' => 'repos#show', :id => /[A-Za-z0-9\.]+?/
+  get '/:username' => 'users#show', :as => :user
+  get '/:username/:repo' => 'repos#show', :repo => /[A-Za-z0-9\.\_\-]+?/, :as => :repo
 
   root :to => 'pages#home'
 
