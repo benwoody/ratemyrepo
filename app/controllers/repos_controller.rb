6410f62bbcf5721c8a23a8597a6bb594cb88138d
Ben Woodall <mail@benwoodall.com>
2013-04-08 13:29:23 -0700

    repos#show links to repo now

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -1,5 +1,6 @@
 class ReposController < ApplicationController
   def show
+    @user = User.find_by_username(params[:username])
     @repo = Repo.find_by_name(params[:repo])
   end
 end
--- spec/integration/list_user_repos_spec.rb
+++ spec/integration/list_user_repos_spec.rb
@@ -15,4 +15,18 @@ feature 'Listing repos' do
     visit "/mctesterson"
     page.should have_content 'test_repo'
   end
+
+end
+
+feature 'Clicking repos' do
+  before do
+    @user = FactoryGirl.create(:user)
+    @repo = FactoryGirl.create(:repo)
+  end
+
+  scenario 'Link should go to repo_path' do
+    visit '/mctesterson/'
+    click_link 'test_repo'
+    page.current_path.should == repo_path(@user,@repo)
+  end
 end
