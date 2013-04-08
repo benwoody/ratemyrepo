d533ac3ea308d6d959bcb24fcb455a4941603fa6
Ben Woodall <mail@benwoodall.com>
2013-04-08 09:52:43 -0700

    testing for user#show to list a users repos

--- config/initializers/github.rb
+++ config/initializers/github.rb
@@ -1,4 +1,3 @@
-
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, GITHUB[:client_id], GITHUB[:client_secret]
 end
--- spec/integration/list_user_repos_spec.rb
+++ spec/integration/list_user_repos_spec.rb
@@ -2,11 +2,17 @@ require 'spec_helper'
 feature 'Listing repos' do
 
   before do
-    login_with :github, user_info
+    FactoryGirl.create(:user)
   end
 
-  scenario "Visit a users page" do
-    visit "/mcawesome"
+  scenario "Visit a users page with no repos" do
+    visit "/mctesterson"
     page.should have_content 'No repos'
+  end
+
+  scenario "Visit a users page with repos" do
+    FactoryGirl.create(:repo)
+    visit "/mctesterson"
+    page.should have_content 'test_repo'
   end
 end
