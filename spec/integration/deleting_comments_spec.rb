553b5fafbe65c5ceee54b42b7f7680ddd15ea01f
Ben Woodall <mail@benwoodall.com>
2013-05-01 14:18:59 -0700

    Testing ownership of a comment.

---
+++ spec/integration/deleting_comments_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+feature "Users can delete comments" do
+  before do
+    FactoryGirl.create(:user)
+    FactoryGirl.create(:repo)
+  end
+
+  scenario "Trying to delete a comment while not signed in" do
+    visit '/mctesterson/test_repo'
+    page.should_not have_link("x")
+  end
+
+  scenario "Delete a comment you own" do
+    login_with :github, user_info
+    visit '/mctesterson/test_repo'
+    fill_in 'comment_body', :with => "Hey this is awesome!"
+    click_button "Post comment"
+    page.should have_link("x", :href => "/comments/1")
+  end
+
+end
