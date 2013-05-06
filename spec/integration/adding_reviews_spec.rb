57547c03ea524c7102e7ea67e1d9211676313df8
Ben Woodall <mail@benwoodall.com>
2013-05-06 14:28:30 -0700

    Reviews are being POSTed twice.
    Submitting a Review makes it show up 4 times in the view. A refresh just
    shows the 2 POSTed Reviews.

--- spec/integration/adding_reviews_spec.rb
+++ spec/integration/adding_reviews_spec.rb
@@ -9,7 +9,7 @@ feature "Adding a Review" do
   # User must sign in/up to review
   scenario "while not logged in" do
     visit '/mctesterson/test_repo'
-    page.should have_content('Sign in to Rate this Repo!')
+    page.should have_content('Sign in via Github to Rate this Repo!')
   end
 
   scenario "while logged in" do
@@ -32,7 +32,7 @@ feature "Adding a Review" do
     visit '/mctesterson/test_repo'
     page.select("4", :from => 'Rating')
     click_button "Rate"
-    page.should have_content "Review added"
+    page.should have_content "4"
   end
 
   # User adds both Star Rating and Comment
@@ -42,12 +42,13 @@ feature "Adding a Review" do
     page.select("4", :from => 'Rating')
     fill_in "Comment", with: "This repo rocks!"
     click_button "Rate"
-    page.should have_content "Review added"
+    page.should have_content "This repo rocks!"
   end
 
   # User is allowed only 1 review. Adding after
   # Reviewing only #updates the Review.
   scenario "if User has already reviewed" do
+    pending("Move to spec for 'Updating Review")
     login_with :github, user_info
     visit '/mctesterson/test_repo'
     page.should have_content "Update Review"
