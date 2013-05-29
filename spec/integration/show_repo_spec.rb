9a1a682fab99b7b52af90083b842478b597300af
Ben Woodall <mail@benwoodall.com>
2013-05-29 15:51:34 -0700

    Rename show_user_profile with the spex suffix

--- spec/integration/show_repo_spec.rb
+++ spec/integration/show_repo_spec.rb
@@ -10,4 +10,18 @@ feature 'Show a Repo' do
     visit '/mctesterson/test_repo'
     page.should have_content 'test_repo'
   end
+
+  scenario "with 10 or more comments" do
+    add_reviews 11
+    visit '/mctesterson/test_repo'
+    page.should have_content "1 2 Next › Last »"
+  end
+
+  def add_reviews num
+    num.times do |n|
+      id = n+2
+      FactoryGirl.create(:user, id: id)
+      FactoryGirl.create(:review, user_id: id)
+    end
+  end
 end
