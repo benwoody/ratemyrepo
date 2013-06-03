21c2329ad26125d2df0acc5ee783fdb08a496260
Ben Woodall <mail@benwoodall.com>
2013-06-03 14:35:11 -0700

    Added site bullet points.

---
+++ spec/integration/show_homepage_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+feature 'Viewing the rootpage' do
+  background do
+    visit '/'
+  end
+  scenario 'as a guest user' do
+    page.should have_content('Sign in via Github!')
+  end
+  scenario 'while logged in' do
+    login_with :github, user_info
+    visit '/'
+    page.should have_content('
+  end
+end
+
